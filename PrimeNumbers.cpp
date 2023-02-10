0e3682a718516633c95de3b840b3e4bd360fdb6e
alinasagaydak <44638862+alinasagaydak@users.noreply.github.com>
2023-02-10 15:19:04 +0700

    Update PrimeNumbers.cpp

--- PrimeNumbers.cpp
+++ PrimeNumbers.cpp
@@ -1,33 +1,25 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-int Prime(int a)	//возвращает 1 - число простое, 0 - не простое
+int isPrime(int a)	//возвращает 1 - число простое, 0 - не простое
 {
-	int k = 0;
-	int i;
-	for (i = 2; i <= int(sqrt(double(a))); i++)
+	for (int i = 2; i <= int(sqrt(double(a))); i++)
 	{
 		if (a % i == 0) {
-			k = k + 1;
+			return 0;
 		}
 	}
-	if (k >= 1)
-		return 0;
-	else
-		return 1;
+	return 1;
 }
 
 void main() {
 	int N = 0;
-	int P;
-	int j;
 	printf("Enter N: ");
 	scanf_s("%d", &N);
 
-	for (j = 2; j <= N; j++)
+	for (int num = 2; num <= N; num++)
 	{
-		P = Prime(j);
-		if (P == 1)
-			printf("%d\n", j);
+		if (isPrime(num) == 1)
+			printf("%d\n", num);
 	}
 }
