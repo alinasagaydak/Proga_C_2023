3e512c32618c1b18b217de9b5e374bdc3e0e5732
alinasagaydak <44638862+alinasagaydak@users.noreply.github.com>
2023-04-13 23:02:40 +0700

    Update matrix_3x3_3.0.cpp

--- matrix_3x3_3.0.cpp
+++ matrix_3x3_3.0.cpp
@@ -1,19 +1,19 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
 
 typedef struct
 {
 	float data[3][3];
-	int width;
-	int height;
+	int xSize;
+	int ySize;
 } Matrix;
 
-void print_matrix(Matrix* M)
+void printMatrix(Matrix* M)
 {
-	for (int row_num = 0; row_num < M->height; row_num++)
+	for (int row_num = 0; row_num < M->ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < M->width; col_num++)
+		for (int col_num = 0; col_num < M->xSize; col_num++)
 		{
 			printf("%f\t", M->data[row_num][col_num]);
 		}
@@ -22,16 +22,16 @@ void print_matrix(Matrix* M)
 	printf("\n");
 }
 
-Matrix READ_mat(char* name, int width, int height)
+Matrix readFileToMatrix(char* name, int xSize, int ySize)
 {
 	Matrix A;
-	A.width = width;
-	A.height = height;
+	A.xSize = xSize;
+	A.ySize = ySize;
 	FILE* fp;
 	fopen_s(&fp, name, "r");
-	for (int row_num = 0; row_num < A.height; row_num++)
+	for (int row_num = 0; row_num < A.ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < A.width; col_num++)
+		for (int col_num = 0; col_num < A.xSize; col_num++)
 		{
 			fscanf_s(fp, "%f", &A.data[row_num][col_num]);
 		}
@@ -40,75 +40,27 @@ Matrix READ_mat(char* name, int width, int height)
 	return A;
 }
 
-void WRITE_to_file(void* A, int operation)
+void writeTextToFile(FILE* targetFile, Matrix* A)
 {
-	FILE* fp;
-
-	if (sizeof(A) == 4)
+	for (int row_num = 0; row_num < A->ySize; row_num++)
 	{
-		fopen_s(&fp, "mat_res.txt", "a+");
+		for (int col_num = 0; col_num < A->xSize; col_num++)
 		{
-			fprintf(fp, "%c", "\n");
-			fprintf(fp, "%s", "Det A:");
-			fprintf(fp, "%f", *(float*)A);
+			fprintf(targetFile, "%f\t", A->data[row_num][col_num]);
 		}
-		fclose(fp);
-	}
-
-	else
-	{
-		switch (operation)
-		{
-		case 1:
-			fopen_s(&fp, "mat_res.txt", "a+");
-			fprintf(fp, "%c", "\n");
-			fprintf(fp, "%s", "A + B");
-			fclose(fp);
-			break;
-		case 2:
-			fopen_s(&fp, "mat_res.txt", "a+");
-			fprintf(fp, "%c", "\n");
-			fprintf(fp, "%s", "A x B");
-			fclose(fp);
-			break;
-		case 3:
-			fopen_s(&fp, "mat_res.txt", "a+");
-			fprintf(fp, "%c", "\n");
-			fprintf(fp, "%s", "transposed A");
-			fclose(fp);
-			break;
-		case 4:
-			fopen_s(&fp, "mat_res.txt", "a+");
-			fprintf(fp, "%c", "\n");
-			fprintf(fp, "%s", "inverse A");
-			fclose(fp);
-			break;
-		}
-
-		fopen_s(&fp, "mat_res.txt", "a+");
-		fprintf(fp, "%c", "\n");
-		for (int row_num = 0; row_num < ((Matrix*)A)->height; row_num++)
-		{
-			for (int col_num = 0; col_num < ((Matrix*)A)->width; col_num++)
-			{
-				fprintf(fp, "%f\t", ((Matrix*)A)->data[row_num][col_num]);
-			}
-			fprintf(fp, "%c", "\n");
-		}
-		//fprintf(fp, "%c", "\n");
-		fclose(fp);
+		fprintf(targetFile, "%c", "\n");
 	}
 }
 
-Matrix summ(Matrix* A, Matrix* B)
+Matrix matrixSumm(Matrix* A, Matrix* B)
 {
 	Matrix res;
-	res.width = A->width;
-	res.height = A->height;
+	res.xSize = A->xSize;
+	res.ySize = A->ySize;
 	
-	for (int row_num = 0; row_num < A->height; row_num++)
+	for (int row_num = 0; row_num < A->ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < A->width; col_num++)
+		for (int col_num = 0; col_num < A->xSize; col_num++)
 		{
 			res.data[row_num][col_num] = A->data[row_num][col_num] + B->data[row_num][col_num];
 		}
@@ -116,24 +68,24 @@ Matrix summ(Matrix* A, Matrix* B)
 	return res;
 }
 
-Matrix multiplication(Matrix* A, Matrix* B)
+Matrix matrixMulti(Matrix* A, Matrix* B)
 {
 	Matrix res;
-	res.width = B->width;
-	res.height = A->height;
-	for (int row_num = 0; row_num < res.height; row_num++)
+	res.xSize = B->xSize;
+	res.ySize = A->ySize;
+	for (int row_num = 0; row_num < res.ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < res.width; col_num++)
+		for (int col_num = 0; col_num < res.xSize; col_num++)
 		{
 			res.data[row_num][col_num] = 0;
 		}
 	}
 
-	for (int row_num = 0; row_num < A->height; row_num++)
+	for (int row_num = 0; row_num < A->ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < A->width; col_num++)
+		for (int col_num = 0; col_num < A->xSize; col_num++)
 		{
-			for (int k = 0; k < A->width; k++)
+			for (int k = 0; k < A->xSize; k++)
 			{
 				res.data[row_num][col_num] += A->data[row_num][k] * B->data[k][col_num];
 			}
@@ -142,15 +94,15 @@ Matrix multiplication(Matrix* A, Matrix* B)
 	return res;
 }
 
-Matrix transpose(Matrix* A)
+Matrix matrixTranspose(Matrix* A)
 {
 	Matrix res;
-	res.width = A->width;
-	res.height = A->height;
+	res.xSize = A->xSize;
+	res.ySize = A->ySize;
 
-	for (int col_num = 0; col_num < res.width; col_num++) //фиксируем столбец и печатаем элементы этого столбца в строчку
+	for (int col_num = 0; col_num < res.xSize; col_num++) //фиксируем столбец и печатаем элементы этого столбца в строчку
 	{
-		for (int row_num = 0; row_num < res.height; row_num++)
+		for (int row_num = 0; row_num < res.ySize; row_num++)
 		{
 			res.data[row_num][col_num] = A->data[col_num][row_num];
 			//fprintf(fp, "%f", res.data[row_num][col_num]); ///вопрос: почему в файл записывает НЕ транспонированную матрицу, а исходную
@@ -161,32 +113,32 @@ Matrix transpose(Matrix* A)
 	return res;
 }
 
-float findDet(Matrix* A)
+float matrixDeterminant(Matrix* A)
 {
 	double reduction;
 	float det = 1;
 
 	Matrix res;
-	res.width = A->width;
-	res.height = A->height;
-	for (int row_num = 0; row_num < A->height; row_num++)
+	res.xSize = A->xSize;
+	res.ySize = A->ySize;
+	for (int row_num = 0; row_num < A->ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < A->width; col_num++)
+		for (int col_num = 0; col_num < A->xSize; col_num++)
 		{
 			res.data[row_num][col_num] = A->data[row_num][col_num];
 		}
 	}
 
 	//приведение матрицы к ступенчатому виду методом гаусса
-	for (int diag_el = 0; diag_el < A->width - 1; diag_el++)
+	for (int diag_el = 0; diag_el < A->xSize - 1; diag_el++)
 	{
-		for (int row_num = diag_el + 1; row_num < A->width; row_num++) // i row_num	j col_num	k diag_el
+		for (int row_num = diag_el + 1; row_num < A->xSize; row_num++) // i row_num	j col_num	k diag_el
 		{
 			if (res.data[diag_el][diag_el] != 0)
 			{
 				//reduction is ...
 				reduction = - res.data[row_num][diag_el] / res.data[diag_el][diag_el];
-				for (int col_num = 0; col_num < A->width; col_num++)
+				for (int col_num = 0; col_num < A->xSize; col_num++)
 				{
 					res.data[row_num][col_num] += res.data[diag_el][col_num] * reduction;
 				}
@@ -205,23 +157,23 @@ float findDet(Matrix* A)
 	return det;
 }
 
-Matrix inversion(Matrix* A) //A N
+Matrix matrixInversion(Matrix* A) //A N
 {
 	double temp;
 	Matrix res;
-	res.width = A->width;
-	res.height = A->height;
-	for (int row_num = 0; row_num < A->height; row_num++)
+	res.xSize = A->xSize;
+	res.ySize = A->ySize;
+	for (int row_num = 0; row_num < A->ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < A->width; col_num++)
+		for (int col_num = 0; col_num < A->xSize; col_num++)
 		{
 			res.data[row_num][col_num] = A->data[row_num][col_num];
 		}
 	}
 
-	for (int i = 0; i < res.height; i++)
+	for (int i = 0; i < res.ySize; i++)
 	{
-		for (int j = 0; j < res.width; j++)
+		for (int j = 0; j < res.xSize; j++)
 		{
 			res.data[i][j] = 0.0;
 			if (i == j)
@@ -229,21 +181,21 @@ Matrix inversion(Matrix* A) //A N
 		}
 	}
 		
-	for (int k = 0; k < res.width; k++)
+	for (int k = 0; k < res.xSize; k++)
 	{
 		temp = A->data[k][k];
 
-		for (int j = 0; j < res.width; j++)
+		for (int j = 0; j < res.xSize; j++)
 		{
 			A->data[k][j] /= temp;
 			res.data[k][j] /= temp;
 		}
 
-		for (int i = k + 1; i < res.width; i++)
+		for (int i = k + 1; i < res.xSize; i++)
 		{
 			temp = A->data[i][k];
 
-			for (int j = 0; j < res.width; j++)
+			for (int j = 0; j < res.xSize; j++)
 			{
 				A->data[i][j] -= A->data[k][j] * temp;
 				res.data[i][j] -= res.data[k][j] * temp;
@@ -251,13 +203,13 @@ Matrix inversion(Matrix* A) //A N
 		}
 	}
 
-	for (int k = res.width - 1; k > 0; k--)
+	for (int k = res.xSize - 1; k > 0; k--)
 	{
 		for (int i = k - 1; i >= 0; i--)
 		{
 			temp = A->data[i][k];
 
-			for (int j = 0; j < res.width; j++)
+			for (int j = 0; j < res.xSize; j++)
 			{
 				A->data[i][j] -= A->data[k][j] * temp;
 				res.data[i][j] -= res.data[k][j] * temp;
@@ -265,18 +217,18 @@ Matrix inversion(Matrix* A) //A N
 		}
 	}
 
-	for (int i = 0; i < res.width; i++)
+	for (int i = 0; i < res.xSize; i++)
 	{
-		for (int j = 0; j < res.width; j++)
+		for (int j = 0; j < res.xSize; j++)
 		{
 			A->data[i][j] = res.data[i][j];
 		}
 	}
 
 	//fprintf(fp, "%s\n", "inverse A:");
-	for (int row_num = 0; row_num < res.height; row_num++)
+	for (int row_num = 0; row_num < res.ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < res.width; col_num++)
+		for (int col_num = 0; col_num < res.xSize; col_num++)
 		{
 			//fprintf(fp, "%f\t", res.data[row_num][col_num]);
 		}
@@ -289,55 +241,76 @@ Matrix inversion(Matrix* A) //A N
 int main()
 {
 	Matrix A, B;
-	int width = 3;
-	int height = 3;
+	int xSize = 3;
+	int ySize = 3;
 
 	//initialize mat A
 	char f_name_A[] = "mat_A.txt";
-	A = READ_mat(f_name_A, width, height);
-	A.width = width;
-	A.height = height;
+	A = readFileToMatrix(f_name_A, xSize, ySize);
+	A.xSize = xSize;
+	A.ySize = ySize;
 
 	//initialize mat B
 	time_t t;
 	srand((unsigned)time(&t));
-	for (int row_num = 0; row_num < height; row_num++)
+	for (int row_num = 0; row_num < ySize; row_num++)
 	{
-		for (int col_num = 0; col_num < width; col_num++)
+		for (int col_num = 0; col_num < xSize; col_num++)
 		{
 			B.data[row_num][col_num] = rand() % 10;	//(201) + (-100);
 		}
 	}
-	B.width = width;
-	B.height = height;
+	B.xSize = xSize;
+	B.ySize = ySize;
 
+	//results of code
+	Matrix resSumm = matrixSumm(&A, &B);
+	Matrix resMulti = matrixMulti(&A, &B);
+	Matrix resTranspose = matrixTranspose(&A);
+	float resDeterminant = matrixDeterminant(&A);
+	Matrix resInsversion = matrixInversion(&A);
+
+	//stdout
 	printf("Matrix A: \n");
-	print_matrix(&A);
+	printMatrix(&A);
 	printf("Matrix B: \n");
-	print_matrix(&B);
+	printMatrix(&B);
 	
 	printf("A + B: \n");
-	Matrix Sum = summ(&A, &B);
-	print_matrix(&Sum);
-	WRITE_to_file(&Sum, 1);
+	printMatrix(&resSumm);
 
 	printf("A x B: \n");
-	Matrix multi = multiplication(&A, &B);
-	print_matrix(&multi);
-	WRITE_to_file(&multi, 2);
+	printMatrix(&resMulti);
 
-	printf("Transpose matrix A: \n");
-	Matrix trans = transpose(&A);
-	print_matrix(&trans);
-	WRITE_to_file(&trans, 3);
+	printf("Transposed matrix A: \n");
+	printMatrix(&resTranspose);
 
-	float det = findDet(&A);
-	printf("Determinant A: %f\n", det);
-	printf("\n");
-	WRITE_to_file(&det, 0);
+	printf("Determinant A: %f\n", resDeterminant);
 
 	printf("Inversion A: \n");
-	Matrix ins = inversion(&A);
-	print_matrix(&ins);
-	WRITE_to_file(&ins, 4);
+	printMatrix(&resInsversion);
+
+	//stdin
+	FILE* fp;
+	fopen_s(&fp, "mat_res.txt", "a+");
+	{
+		fprintf(fp, "%s\n", "A + B:");
+		writeTextToFile(fp, &resSumm);
+		//fprintf(fp, "%c", "\n");
+
+		fprintf(fp, "%s\n", "A x B:");
+		writeTextToFile(fp, &resMulti);
+		//fprintf(fp, "%c", "\n");
+
+		fprintf(fp, "%s\n", "Transposed A:");
+		writeTextToFile(fp, &resTranspose);
+		//fprintf(fp, "%c", "\n");
+
+		fprintf(fp, "%f\n", resDeterminant);
+
+		fprintf(fp, "%s\n", "Inverse A:");
+		writeTextToFile(fp, &resInsversion);
+		//fprintf(fp, "%c", "\n");
+	}
+	fclose(fp);
 }
